015bd8769fe29fee40d634629e9311dd54e06a37
sadaqathussain8190 <153924398+sadaqathussain8190@users.noreply.github.com>
2025-04-25 21:51:19 +0500

    Update Pro_02_Password_Strength_Meter.py

--- Pro_02_Password_Strength_Meter.py
+++ Pro_02_Password_Strength_Meter.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyM4xaLDjtjJ+zHbKUiEDbc7",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
